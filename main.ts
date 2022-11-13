037177bd1c38a3cda1387299e326f92c3128d556
superng <118038158+superng@users.noreply.github.com>
2022-11-13 17:31:13 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-
+let col = 0
+let row = 0
+basic.showIcon(IconNames.Ghost)
+basic.forever(function () {
+    for (let index = 0; index < 4; index++) {
+        if (input.soundLevel() > 128) {
+            row = randint(0, 4)
+            col = randint(0, 4)
+        }
+        if (led.point(col, row)) {
+            led.unplot(col, row)
+            led.plot(col + 1, row)
+        }
+    }
+})
